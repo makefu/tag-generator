e7c62ab1cb21e068ee3ab200e075023978590a16
incept <makefu@incept.krebsco.de>
2012-06-10 11:17:35 +0000

    refactoring, add cutie qr generator

--- application/init.py
+++ application/init.py
@@ -1,7 +1,6 @@
 from flask import Flask,url_for,render_template,request,abort,make_response,send_file,redirect
-
-app = Flask(__name__)
-print app.config.keys()
+app = Flask(__name__) 
+app.config["SERVER_NAME"] = "incept.krebsco.de:8080"
 
 # in-memory database
 db = { "box" : [], "project": []}
@@ -14,23 +13,85 @@ def hello():
 def publish():
   """
   """
-  owner = request.form["owner-name"]
-  email= request.form["owner-email"]
-  t = request.form["item-type"]
-  url = request.form["wiki-url"]
-  text = request.form["freetext"]
+  try:
+    owner = request.form["owner-name"]
+    email= request.form["owner-email"]
+    twitter = request.form["owner-twitter"]
+    t = request.form["item-type"]
+    url = request.form["wiki-url"]
+    text = request.form["freetext"]
+  except:
+    abort(500)
   if t == "item-type-project":
-    ident = len(db["project"])
-    db["project"].append({"owner": owner, "email": email,"ident": ident,"url":url,"text":text})
-    print "Add a project for Owner %s (%s) with url `%s` and freetext `%s`" % (owner,email,url,text)
-    return redirect("/project/details/%d" % ident)
+    t = "project"
   elif t == "item-type-box":
-    ident = len(db["box"])
-    db["box"].append({"owner": owner, "email": email,"ident": ident})
-    print "Add box with id `%d` and owner %s(%s)" % (ident,owner,email)
-    return redirect("/box/details/%d" % ident)
+    t = "box"
   else:
     return "WTF?"
+  ident = len(db[t])
+  db[t].append({"owner": owner, "email": email,"ident": ident,"url":url,"text":text, "twitter" : twitter})
+  return redirect("/%s/details/%d" % (t,ident))
+
+
+@app.route("/<typ>/qr/<int:ident>")
+def gen_qr(typ=None,ident=None):
+  if ident is None: abort (500)
+
+  if typ not in ["box","project"]: abort(404)
+
+  data = {}
+  try: data = db[typ][ident]
+  except : abort(404)
+
+  import qrcode
+  import os.path
+  qrpath= "qr/%s"%ident
+
+  #if not os.path.isfile(qrpath) or True:
+  qr = qrcode.QRCode(version=5,error_correction=qrcode.constants.ERROR_CORRECT_Q,box_size=15,border=0)
+
+  qr.add_data("%s/%s/%s"%(app.config["SERVER_NAME"],typ,ident))
+  qr.make(fit=True)
+  img = qr.make_image()
+  img.save(qrpath)
+  from PIL import Image, ImageDraw, ImageFont
+
+  heading = "A Hacker known as %s" % (data["owner"])
+  emailtext = "Email: %s" %data["email"]
+  twittertext= "Twitter: %s" %data["twitter"]
+  urltext = "URL: %s" %data["url"]
+  freitext = "%s"%data["text"]
+
+  im = Image.open("../resources/A6_300dpi.png")
+  qr = Image.open(qrpath)
+  qr.convert('RGB')
+  im.convert('RGB')
+  headingFontsize = 70
+  headingFont = ImageFont.truetype("../resources/arialbd.ttf", headingFontsize)
+  textFontsize = 40
+  textFont = ImageFont.truetype("../resources/arial.ttf", textFontsize)
+  offsetX = 340  #offset for the shackspace logo on the leftside
+  draw = ImageDraw.Draw(im)
+  #Heading
+  headingWidth, headingHeight = draw.textsize(heading, font=headingFont)
+  draw.text((((im.size[0]- offsetX)/2) - (headingWidth/2 - offsetX), im.size[1]/8), heading, font=headingFont, fill="#000000") 
+  #Text
+  textWidth, textHeight = draw.textsize(emailtext, font=textFont)
+  draw.text((((im.size[0]- offsetX)/2) - (textWidth/2 - offsetX), im.size[1]/5), emailtext, font=textFont, fill="#000000") 
+  draw.text((((im.size[0]- offsetX)/2) - (textWidth/2 - offsetX), (im.size[1]/5)+textHeight), twittertext, font=textFont, fill="#000000") 
+  draw.text((((im.size[0]- offsetX)/2) - (textWidth/2 - offsetX), (im.size[1]/5)+2*textHeight), urltext, font=textFont, fill="#000000") 
+  draw.text((((im.size[0]- offsetX)/2) - (textWidth/2 - offsetX), (im.size[1]/5)+4*textHeight), freitext, font=textFont, fill="#000000") 
+  #QR-Code
+  draw.bitmap(((im.size[0]-offsetX)/2 - (qr.size[0]/2 - offsetX), im.size[1]/2.2), qr, fill="#000000")
+  del draw
+  im.save(qrpath, "PNG")
+
+  assert (os.path.isfile(qrpath))
+  f = open(qrpath)
+  #response = make_response(f.read())
+  #f.close()
+  #response.headers["Content-Type"] = "image/png"
+  return send_file(f,mimetype="image/png")
 
 @app.route("/box/details/<int:ident>")
 def handle_box(ident=None):
@@ -52,30 +113,8 @@ def handle_project(ident=None):
   except : print "Project %d not found" % ident
   return render_template("project.html",app=app,ident=ident,data=data)
 
-@app.route("/qr/<typ>/<int:ident>")
-def gen_qr(typ=None,ident=None):
-  import qrcode
-  import os.path
-  if ident is None:
-    abort (500)
-  if typ not in ["box","project"]:
-    abort(404)
-  qrpath= "qr/%s"%ident
-  if not os.path.isfile(qrpath):
-    img = qrcode.make("%s/%s/%s"%(app.config["SERVER_NAME"],typ,ident))
-    img.save(qrpath)
-
-  assert (os.path.isfile(qrpath))
-  f = open(qrpath)
-  #response = make_response(f.read())
-  #f.close()
-  #response.headers["Content-Type"] = "image/png"
-  return send_file(f,mimetype="image/png")
-
-
 if __name__ == "__main__":
   #url_for('static', filename='index.js')
   #url_for('static', filename='jquery-1.7.2.min.js')
   app.debug = True
-  app.config["SERVER_NAME"] = "incept.krebsco.de:8080"
   app.run(host= "0.0.0.0",port=8080)
